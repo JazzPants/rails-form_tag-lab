c95be1157dc6b34a02d7edf4f13765ef2433e6b4
Leighton <leighton.l14@gmail.com>
2022-03-10 01:18:28 +1100

    Finished!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    puts params[:student]
+    redirect_to action: "index"
+  end
+
   def index
     @students = Student.all
   end
